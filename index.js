03ae1fd327fd867e1a4a854dbc99ed306bafa200
Hala AlArid <halaalarid99@gmail.com>
2022-11-03 01:27:57 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+const Data = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+ 
+//   send data
+
+ const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(Data),
+  };
+  
+
+    function submitData(name, email){
+
+        return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object);
+          object.name = name;
+          object.email = email;
+        })
+        .catch(function (error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
+    }
+
+      
+
